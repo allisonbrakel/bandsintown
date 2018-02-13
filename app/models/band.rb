e1a8bf081004e8976c6ea614acf77e925f70fd1d
Allison Brakel <abrakel135@gmail.com>
2018-02-13 02:01:44 +0000

    Added validations for Band

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,6 @@
 class Band < ApplicationRecord
   has_many :events
-
-  validates :name, :event_count, presence:  true
+  validates :name, :event_count, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :event_count, numericality: true
 end
